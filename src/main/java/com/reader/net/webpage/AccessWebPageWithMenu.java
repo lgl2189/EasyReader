a6023b7f37c4fa362a0ae4d3f96229ef1e10be3a
lgl2189 <1214525891@qq.com>
2025-04-08 14:36:15 +0800

    修改菜单项的action类型为Consumer，并修改相关逻辑

--- src/main/java/com/reader/net/webpage/AccessWebPageWithMenu.java
+++ src/main/java/com/reader/net/webpage/AccessWebPageWithMenu.java
@@ -5,18 +5,14 @@ import com.reader.ui.util.JavaScriptUtil;
 import com.reader.util.IdGenerator;
 import javafx.application.Platform;
 import javafx.concurrent.Worker;
-import javafx.event.ActionEvent;
-import javafx.scene.control.Alert;
-import javafx.scene.input.MouseButton;
-import javafx.scene.input.MouseEvent;
 import javafx.scene.web.WebEngine;
 import javafx.scene.web.WebView;
 import netscape.javascript.JSObject;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.function.Consumer;
 import java.util.stream.Collectors;
 
 /**
@@ -69,7 +65,7 @@ public abstract class AccessWebPageWithMenu extends AccessWebPage {
             window.setMember("javaBridge", javaBridge);
             Gson gson = new Gson();
             String jsonMenuItems = gson.toJson(menuItemList.stream()
-                    .map(item -> Map.of("id", item.id(), "title", item.title()))
+                    .map(item -> Map.of("id", item.getId(), "title", item.getTitle()))
                     .collect(Collectors.toList()));
             webEngine.executeScript("window.javaMenuItems = JSON.parse('" + jsonMenuItems + "')");
             webEngine.executeScript(menuScript);
@@ -78,54 +74,115 @@ public abstract class AccessWebPageWithMenu extends AccessWebPage {
 
     /**
      * 为浏览器自定义右键菜单添加菜单项
-     * @param title 菜单项标题
+     *
+     * @param title  菜单项标题
      * @param action 菜单项点击事件，菜单项被点击时会触发该事件
+     * @return 菜单项 ID
      */
-    public void addMenuItem(String title, javafx.event.EventHandler<ActionEvent> action) {
+    public String addMenuItem(String title, Consumer<String> action) {
         if (isStart) {
             throw new IllegalCallerException("请在调用run()之前添加菜单项！");
         }
         MenuItem menuItem = new MenuItem(IdGenerator.generateIdFromString(title), title, action);
         menuItemList.add(menuItem);
+        return menuItem.getId();
     }
 
     /**
      * 为浏览器自定义右键菜单添加菜单项，该菜单项仅用于UI操作，action将在UI线程中执行
-     * @param title 菜单项标题
+     *
+     * @param title  菜单项标题
      * @param action 菜单项点击事件，菜单项被点击时会触发该事件
+     * @return 菜单项 ID
      */
-    public void addMenuItemForUiOperator(String title, javafx.event.EventHandler<ActionEvent> action) {
-        javafx.event.EventHandler<ActionEvent> uiAction = event -> {
-            Platform.runLater(()->{
-                action.handle(event);
-            });
-        };
-        addMenuItem(title, uiAction);
+    public String addMenuItemForUiOperator(String title, Consumer<String> action) {
+        Consumer<String> uiAction = event -> Platform.runLater(() -> action.accept(event));
+        return addMenuItem(title, uiAction);
+    }
+
+    /**
+     * 设置给定menuItemId菜单项的属性，包括标题、点击事件处理方法。该方法需要在run()方法之前调用。
+     *
+     * @param menuItemId 菜单项 ID
+     * @param title      菜单项标题
+     * @param action     菜单项点击事件处理函数
+     */
+    public void setMenuItemProperties(String menuItemId, String title, Consumer<String> action) {
+        menuItemList.stream()
+                .filter(item -> item.getId().equals(menuItemId))
+                .findFirst()
+                .ifPresent(item -> {
+                    item.setTitle(title);
+                    item.setXpathHandler(action);
+                });
     }
 
     protected class JavaBridge implements AccessWebPage.JavaBridge {
         /**
          * 菜单项点击事件处理函数，该处理函数默认实现会直接调用菜单项的 action 事件处理函数，并在 UI 线程中执行
          * 如果不需要立刻执行 action 事件处理函数，可以重写该函数，在其中执行必要的操作，然后调用 handleAction 方法来执行 action 事件处理函数
+         *
          * @param menuItemId 菜单项 ID
-         * @param elementInfoJson 选择的元素信息的 JSON 字符串
+         * @param xpath      选择的元素的xpath路径
          */
-        public void onMenuItemClicked(String menuItemId, String elementInfoJson) {
-            handleAction(menuItemId);
+        public void onMenuItemClicked(String menuItemId, String xpath) {
+            handleAction(menuItemId, xpath);
         }
 
-        protected void handleAction(String menuItemId) {
+        protected void handleAction(String menuItemId, String xpath) {
             menuItemList.stream()
-                    .filter(item -> item.id().equals(menuItemId))
+                    .filter(item -> item.getId().equals(menuItemId))
                     .findFirst()
-                    .ifPresent(item -> {
-                        Platform.runLater(() -> {
-                            item.action().handle(new ActionEvent());
-                        });
-                    });
+                    .ifPresent(item -> Platform.runLater(() -> item.getXpathHandler().accept(xpath)));
         }
     }
 
-    protected record MenuItem(String id, String title, javafx.event.EventHandler<ActionEvent> action) {
+    protected static class MenuItem {
+        private String id;
+        private String title;
+        private Consumer<String> xpathHandler;
+
+        public MenuItem() {
+        }
+
+
+        public MenuItem(String id, String title, Consumer<String> xpathHandler) {
+            this.id = id;
+            this.title = title;
+            this.xpathHandler = xpathHandler;
+        }
+
+        public String getId() {
+            return id;
+        }
+
+        public void setId(String id) {
+            this.id = id;
+        }
+
+        public String getTitle() {
+            return title;
+        }
+
+        public void setTitle(String title) {
+            this.title = title;
+        }
+
+        public Consumer<String> getXpathHandler() {
+            return xpathHandler;
+        }
+
+        public void setXpathHandler(Consumer<String> xpathHandler) {
+            this.xpathHandler = xpathHandler;
+        }
+
+        @Override
+        public String toString() {
+            return "MenuItem{" +
+                    "id='" + id + '\'' +
+                    ", title='" + title + '\'' +
+                    ", xpathHandler=" + xpathHandler +
+                    '}';
+        }
     }
 }
