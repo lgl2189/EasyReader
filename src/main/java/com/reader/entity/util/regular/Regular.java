55f08f72b26ef96f832b40a5fed31f32e76543ba
lgl2189 <1214525891@qq.com>
2025-04-08 23:56:51 +0800

    添加Regular接口，用于表示一个公式

---
+++ src/main/java/com/reader/entity/util/regular/Regular.java
@@ -0,0 +1,19 @@
+package com.reader.entity.util.regular;
+
+import java.io.Serial;
+import java.io.Serializable;
+
+/**
+ * @author ：李冠良
+ * @description ：Regular 接口，实现这个接口，表示这个类表示一种公式。
+ * @date ：2025 4月 08 23:48
+ */
+
+public interface Regular {
+    /**
+     * 应用规则
+     * @param input 输入
+     * @return 输出
+     */
+    Object applyRule(Object input);
+}
